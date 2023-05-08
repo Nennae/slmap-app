82baf5b23a83bf1994ae965be7a9796f3ef8722f
Sehnaz Erdal <sehnazerdal@MacBook-Pro-2.lan>
2023-05-08 20:12:54 +0200

    added pbf library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,16 +8,17 @@ import {
       Popup
 } 
 from 'react-leaflet';
-import L from 'leaflet';
+// import { L, Icon, divIcon } from 'leaflet';
+// import MarkerClusterGroup from 'react-leaflet-cluster';
 
 const Map = () => {
-      delete L.Icon.Default.prototype._getIconUrl;
+//       delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-      iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-      iconUrl: require("leaflet/dist/images/marker-icon.png"),
-      shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
+// L.Icon.Default.mergeOptions({
+//       iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+//       iconUrl: require("leaflet/dist/images/marker-icon.png"),
+//       shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+// });
       // const map = useMap();
       // const mapEvent = useMapEvents({
       //       click: () => {
@@ -31,6 +32,14 @@ L.Icon.Default.mergeOptions({
       const position = [59.3265, 18.0644]
       const trafiklab_api_key = "a2242a4330664e1ba8179c3cb677f9ff";
 
+      // const createCustomClusterIcon = (cluster) => {
+      //       return L.divIcon({
+      //             html: `<span>${cluster.getChildCount()}</span>`,
+      //             className: 'marker-cluster-custom',
+      //             iconSize: L.point(40, 40, true),
+      //       });
+      // }
+
 return (
       <MapContainer
       center={position} 
@@ -41,9 +50,14 @@ return (
             <TileLayer
       url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      {/* <MarkerClusterGroup
+      chunkedLoading
+      iconCreateFunction={createCustomClusterIcon}
+      >
       <Marker position={position}>
             <Popup>My popup!<br/>yes my popup!</Popup>
       </Marker>
+      </MarkerClusterGroup> */}
       </MapContainer>
 )
 }
