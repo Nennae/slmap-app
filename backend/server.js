07324da8f4aa43973b442d15db46b0fb0a4d0e75
Nennae <sezenerdal@outlook.com>
2023-05-13 21:06:04 +0200

    added new db with trips+routes collections to mongodb

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 
+
 const app = express();
 
 const port = process.env.PORT || 3001;
@@ -20,3 +21,10 @@ app.get("/api", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+// require database connection 
+const dbConnect = require("./db/dbConnect");
+
+// execute database connection 
+dbConnect();
+
